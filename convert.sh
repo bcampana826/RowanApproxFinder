e74be3d4a38831cca31d96d1c7ffb424d720478f
Michael Loughrin <loughr95@students.rowan.edu>
2023-03-21 20:57:36 -0400

    added convert script

---
+++ convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# $1 is labels, $2 is edges, $3 is out file
+
+if [[ $# -ne 3 ]]; then
+	echo "faulty input. valid: edge_list, labels, out_file"
+	exit -1
+fi
+
+printf "t 1 " > $3
+
+wc -l < $1 >> $3
+
+awk '{print "v " $1, $2}' $1 >> $3
+
+awk '{print "e " $1, $2}' $2 >> $3
